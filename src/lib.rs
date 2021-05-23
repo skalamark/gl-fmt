cb21ea83922e69039b3d1f30789093462753e41c
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-05-23 20:27:40 -0300

    create struct: Format

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use gl_core::preludes::*;
+
+pub struct Format {
+	hard_tabs: bool,
+	tab_spaces: usize,
+	indent_size: usize,
+}
